fa0134d3d9d57c10fa658d71cdc715b17d80e726
sunbi <minorSunbi@gmail.com>
2021-07-08 19:23:53 +0900

    DeliveryCategory.vue를 CategoryList.vue로 변경 (확장성 향상)

---
+++ src/components/CategoryList.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="flex flex-center q-pa-md q-gutter-lg delivery-category">
+    <div v-for="(item, index) in categories" :key="index">
+      <category-item :img="item.img" :name="item.name" />
+    </div>
+  </div>
+</template>
+
+<script>
+import { defineAsyncComponent } from 'vue'
+export default {
+  components: {
+    CategoryItem: defineAsyncComponent(() => import('components/CategoryItem.vue'))
+  },
+  props: [
+    'categories',
+  ],
+}
+</script>
+
+<style lang="sass" scoped>
+.delivery-category
+  background-color: white
+</style>>
--- src/pages/DeliveryPage.vue
+++ src/pages/DeliveryPage.vue
@@ -1,7 +1,7 @@
 <template>
   <q-page class="">
     <delivery-carousel/>
-    <delivery-category/>
+    <delivery-category :categories="categories"/>
   </q-page>
 </template>
 
@@ -10,10 +10,52 @@ import { defineAsyncComponent } from 'vue'
 export default {
   components: {
     DeliveryCarousel: defineAsyncComponent(() => import('components/DeliveryCarousel.vue')),
-    DeliveryCategory: defineAsyncComponent(() => import('components/DeliveryCategory.vue')),
+    DeliveryCategory: defineAsyncComponent(() => import('src/components/CategoryList.vue')),
   },
   data() {
     return {
+      categories: [
+        {
+          img: 'category',
+          name: '분류'
+        },
+        {
+          img: 'category',
+          name: '분류'
+        },
+        {
+          img: 'category',
+          name: '분류'
+        },
+        {
+          img: 'category',
+          name: '분류'
+        },
+        {
+          img: 'category',
+          name: '분류'
+        },
+        {
+          img: 'category',
+          name: '분류'
+        },
+        {
+          img: 'category',
+          name: '분류'
+        },
+        {
+          img: 'category',
+          name: '분류'
+        },
+        {
+          img: 'horizontal_rule',
+          name: ''
+        },
+        {
+          img: 'horizontal_rule',
+          name: ''
+        },
+      ]
     }
   },
 }
